ecce12e5176c82fdab4b98d374dffa1a9df987ba
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-09-19 02:09:23 +0300

    Scheduled Message Support Added

--- WhatsAppMessageSender/application.py
+++ WhatsAppMessageSender/application.py
@@ -6,8 +6,16 @@ from Business import messageSender
 
 data = sys.stdin.readlines()
 data = json.loads(data[0])
+choose = data[0]['choose']
 path = data[0]['path']
 message = data[0]['message']
-# print("Pyhton Message-> " + message)
-messageSender.send_message_immediately(path, message);
+
+if(choose == 1):
+    messageSender.send_message_immediately(path, message);
+
+if(choose == 2):
+    time_hour = int(data[0]['timeHour'])
+    time_min = int(data[0]['timeMin'])
+    messageSender.send_timed_message(path, message, time_hour, time_min)
+
 sys.stdout.flush()
--- inputSender.js
+++ inputSender.js
@@ -3,32 +3,58 @@ const { PythonShell } = require("python-shell");
 
 const { ipcRenderer } = electron;
 
-let sendImmediatelyBtn = document.querySelector("#sendImmediatelyBtn");
+let completeMessage = "Tamamlandı";
+
 let uploadBtn = document.querySelector("#uploadBtn");
+let timedUploadBtn = document.querySelector("#timedUploadBtn");
+let sendImmediatelyBtn = document.querySelector("#sendImmediatelyBtn");
+let sendTimedBtn = document.querySelector("#sendTimedBtn");
+
 let pathWay = document.querySelector("#pathWay");
-let inputValue = document.querySelector("#message");
+let timedPathWay = document.querySelector("#timedPathWay");
+let instantMessage = document.querySelector("#instantMessage");
+
+let timedMessage = document.querySelector("#timedMessage");
+let timeHour = document.querySelector("#timeHour");
+let timeMin = document.querySelector("#timeMin");
 global.contactValue = "";
 
 uploadBtn.addEventListener("click", () => {
+  contactValue = "";
+  ipcRenderer.send("open-file-dialog-for-file");
+});
+timedUploadBtn.addEventListener("click", () => {
+  contactValue = "";
   ipcRenderer.send("open-file-dialog-for-file");
 });
 
 ipcRenderer.on("selected-file", function (event, path) {
   contactValue = path;
   pathWay.value = contactValue;
+  timedPathWay.value = contactValue;
 });
 
 sendImmediatelyBtn.addEventListener("click", () => {
-  //ipcRenderer.send("key", "DENEME");
   if (contactValue === "") {
-    pathWay.value = "Lütfen Uygun Bir Kişi Listesi Seçiniz!"
+    alert("Lütfen Uygun Bir Kişi Listesi Seçiniz!");
+    pathWay.value = "Lütfen Uygun Bir Kişi Listesi Seçiniz!";
     return;
   }
-  sendMessageInfo(inputValue.value, contactValue);
+  sendMessageInfo(instantMessage.value, contactValue);
+});
+
+sendTimedBtn.addEventListener("click", () => {
+  if (contactValue === "") {
+    alert("Lütfen Uygun Bir Kişi Listesi Seçiniz!");
+    pathWay.value = "Lütfen Uygun Bir Kişi Listesi Seçiniz!";
+    return;
+  }
+  sendMessageInfoWithTime(timedMessage.value, contactValue, timeHour.value, timeMin.value);
 });
 
 function sendMessageInfo(message, path) {
   let messageInfo = {
+    choose: 1,
     message: message,
     path: path,
   };
@@ -45,6 +71,31 @@ function sendMessageInfo(message, path) {
     if (err) {
       throw err;
     }
-    console.log("finished");
+    alert(completeMessage);
+  });
+}
+
+function sendMessageInfoWithTime(message, path, timeHour, timeMin) {
+  let messageInfo = {
+    choose: 2,
+    message: message,
+    path: path,
+    timeHour: timeHour,
+    timeMin: timeMin,
+  };
+
+  let pyshell = new PythonShell("./WhatsAppMessageSender/application.py");
+  //console.log(`JavaScript Tarafı Mesajı-> ${messageInfo.message}`);
+  pyshell.send(JSON.stringify([messageInfo]));
+
+  pyshell.on("message", function (message) {
+    console.log(message);
+  });
+
+  pyshell.end(function (err) {
+    if (err) {
+      throw err;
+    }
+    alert(completeMessage);
   });
 }
